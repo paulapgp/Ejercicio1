2f8dba1703e1f0e4dcd0c0a280dc348ade45940c
paulapgp <usuario@L-1702037>
2017-07-06 18:01:28 +0200

    subo el primer ejercicio

---
+++ Ejercicio1/Principal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public class Principal
+    {
+        public string nombreApellidos { get; set; } = "Paula Garcia Pasalodos";
+        public int edad { get; set; } = 23;
+        public int año { get; set; } = 1994;
+
+    }
+
+    public class Operaciones: Principal
+    {
+        public string CrearMensaje(Principal obj)
+        {
+            string cadena = "";
+
+            cadena = "La persona seleccionada es : " + obj.nombreApellidos + " nacida en el año " + obj.año + " y edad " + obj.edad + " años";
+
+            return cadena;
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio1;
+
+namespace Test
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Operaciones sut = new Operaciones();
+            Principal obj = new Principal();
+            obj.nombreApellidos = "Paula Garcia Pasalodos";
+            obj.edad = 23;
+            obj.año = 1994;
+
+            string cadenaReal = sut.CrearMensaje(obj);
+            string cadenaEsperada = "La persona seleccionada es : Paula Garcia Pasalodos nacida en el año 1994 y edad 23 años";
+
+            Assert.AreEqual(cadenaReal, cadenaEsperada);
+        }
+    }
+}
